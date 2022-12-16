aa55d158112ec38ed122991dba5f2c3f7d1cc2fe
Leo D'Arcy <leo.darcy@PowerONPlatforms.com>
2022-12-16 18:15:45 +0000

    update extension to download the azuresigntool tool in pipeline

--- task/taskSetup.ts
+++ task/taskSetup.ts
@@ -1,8 +1,18 @@
 import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
+import {IExecSyncResult} from 'azure-pipelines-task-lib/toolrunner';
 
 async function run() {
     try {
         console.log('Running Pre Signing Activities');
+
+        var toolPath = tl.which('dotnet');
+        var npm:tr.ToolRunner = tl.tool(toolPath).line('tool install --global azuresigntool');
+        var execResult: IExecSyncResult = npm.execSync();
+        if (execResult.stderr && !execResult.stderr.startsWith("Tool 'azuresigntool' is already installed."))
+        {
+            throw new Error(execResult.stderr)
+        }
     }
     catch (err) {
         if (err instanceof Error) {
