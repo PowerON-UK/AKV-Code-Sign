9649f39cf9c6043915e8a052bbb38bc82faefcf9
Leo D'Arcy <leo.darcy@PowerONPlatforms.com>
2022-12-17 18:34:17 +0000

    Extension is signing files

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # AKV-Code-Sign
 Azure Key Vault Code Signing DevOps Pipeline Extension
+
+## Required Setup
+- Visual Studio Code (Not a hard requirement but all development on this extension at PowerON is done with VS Code)
+- Latest version of Node (VS Pipeline only runs in Node 10 however Mocha needs at least Node 14 to operate)
+- Latest version of Typescript `npm install -g typescript`
+- Latest version of the Azure Devops CLI Tools `npm install -g tfx-cli`
+- Run npm install from the project directory
+
+## Building the Extension
+- update `task/task.json` version number
+- update `vss-extension.json` version number 
+- run `npm install task\` on the project directory
+- run `tsc` on the project directory to compile the Typescript into Javascript
+- run `tfx extension create --manifest-globs vss-extension.json` to compile the extension
+
+### Azure Devops API Reference 
+- Typescript API https://github.com/microsoft/azure-pipelines-task-lib/blob/master/node/docs/azure-pipelines-task-lib.md
+- vss-extension.json https://learn.microsoft.com/en-us/azure/devops/extend/develop/manifest?view=azure-devops
+- task.json https://github.com/Microsoft/azure-pipelines-task-lib/blob/master/tasks.schema.json
+
+## Extension Behavior
+- taskSetup - Install the tool AzureSignTool to the local machine using the dotnet CLI (https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-tool-install)
+- taskRun - Takes pipeline input and converts into a commandline to run the AzureSignTool against the required binaries
+### !Remove or update before publish!
+- taskClosedown - Nothing of any value currently, placeholder in case its needed later 
+
+## Shoutout
+A massive shoutout to [Kevin Jones](https://github.com/vcsjones) for writing the [AzureSignTool](https://github.com/vcsjones/AzureSignTool), without this tool this extension would not be possible 
+
+## Permissions
+To enable the service connector to access the Certificate, ensure that the Key Vault has an Access Control Policy enabling the service connector with the following rights:
+| Area    | Permissions |
+| -------- | ------- |
+| Key  | Verify, Sign, Get, List    |
+| Secret | Get, List     |
+| Certificate    | Get, List    |
--- task/taskRun.ts
+++ task/taskRun.ts
@@ -1,13 +1,54 @@
 import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
+        const azureConnectionName = tl.getInputRequired('AzureConnection');
+
+        const AuthClientID = tl.getEndpointAuthorizationParameterRequired(azureConnectionName, 'serviceprincipalid')
+        const AuthTenantID = tl.getEndpointAuthorizationParameterRequired(azureConnectionName, 'tenantid')
+        const AuthClientSecret = tl.getEndpointAuthorizationParameterRequired(azureConnectionName, 'serviceprincipalkey')
+
+        const KeyVaultURL = tl.getInputRequired('KeyVaultURL');
+        const CertificateName = tl.getInputRequired('CertificateName');
+        const TimeStampURL = tl.getInputRequired('TimeStampURL');
+        const TimeStampHashLevel = tl.getInputRequired('TimeStampHashLevel');
+        const FileHashLevel = tl.getInputRequired('FileHashLevel');
+
+        const FilePath = tl.getPathInputRequired('FilePath');
+
+        const ContinueOnError = tl.getBoolInput('ContinueOnError');
+        const SkipSigned = tl.getBoolInput('SkipSigned');
+
+        var Description = tl.getInput('Description', false)
+        if (Description === undefined)
+        {
+            Description = "" //If not already set create as empty string
         }
-        console.log('Hello', inputString);
+
+        var DescriptionURL = tl.getInput('DescriptionURL', false)
+        if (DescriptionURL === undefined)
+        {
+            DescriptionURL = "" //If not already set create as empty string
+        }
+
+        var fileList = tl.find(FilePath);
+
+        signFiles(
+            KeyVaultURL,
+            AuthClientID,
+            AuthClientSecret,
+            AuthTenantID,
+            CertificateName,
+            TimeStampURL,
+            TimeStampHashLevel,
+            FileHashLevel, 
+            fileList,
+            ContinueOnError, 
+            SkipSigned,
+            Description,
+            DescriptionURL
+        )
     }
     catch (err) {
         if (err instanceof Error) {
@@ -18,5 +59,60 @@ async function run() {
         
     }
 }
+
+//Descriptions taken from AzureSignTool Documentation
+//KeyVaultURL: A fully qualified URL of the key vault with the certificate that will be used for signing. An example value might be https://my-vault.vault.azure.net.
+//AuthClientID: This is the client ID used to authenticate to Azure, which will be used to generate an access token.
+//AuthClientSecret: This is the client secret used to authenticate to Azure, which will be used to generate an access token.
+//AuthTenantID: This is the tenant id used to authenticate to Azure, which will be used to generate an access token. 
+//CertificateName: The name of the certificate used to perform the signing operation.
+//TimeStampURL: A URL to an RFC3161 compliant timestamping service. This parameter serves the same purpose as the /tr option in the Windows SDK signtool.
+//TimeStampHashLevel: The name of the digest algorithm used for timestamping. Possible values: sha1, sha256, sha384, sha512
+//FileHashLevel: The name of the digest algorithm used for hashing the file being signed. Possible values: sha1, sha256, sha384, sha512
+//ContinueOnError: If multiple files to sign are specified, this flag will cause the signing process to move on to the next file when signing fails.
+//SkipSigned: If a file is already signed it will be skipped, rather than replacing the existing signature.
+//Description: Optional - A description of the signed content. This parameter serves the same purpose as the /d option in the Windows SDK signtool. If this parameter is not supplied, the signature will not contain a description.
+//DescriptionURL: Optional - A URL with more information of the signed content. This parameter serves the same purpose as the /du option in the Windows SDK signtool. If this parameter is not supplied, the signature will not contain a URL description.
+
+async function signFiles(
+    KeyVaultURL : string,
+    AuthClientID: string,
+    AuthClientSecret : string,
+    AuthTenantID : string,
+    CertificateName : string,
+    TimeStampURL : string,
+    TimeStampHashLevel : string,
+    FileHashLevel : string,
+    FileList: string[],
+    ContinueOnError : boolean,
+    SkipSigned : boolean,
+    Description : string,
+    DescriptionURL : string
+    )
+{
+
+    var toolPath = tl.which('azuresigntool');
+    var azuresigntool:tr.ToolRunner = tl.tool(toolPath)
+        .arg('sign')    
+        .arg('--azure-key-vault-url ' + KeyVaultURL)
+        .arg('--azure-key-vault-client-id ' + AuthClientID)
+        .arg('--azure-key-vault-client-secret ' + AuthClientSecret)
+        .arg('--azure-key-vault-tenant-id ' + AuthTenantID)
+        .arg('--azure-key-vault-certificate ' + CertificateName)
+        .arg('--timestamp-rfc3161 ' + TimeStampURL)
+        .arg('--timestamp-digest ' + TimeStampHashLevel)
+        .arg('--file-digest ' + FileHashLevel)
+        .arg(FileList)
+        .arg('--verbose')
+        .argIf(!isEmptyString(Description),'--description ' + Description)
+        .argIf(!isEmptyString(DescriptionURL),'--description ' + DescriptionURL)
+        .argIf(ContinueOnError,'--continue-on-error')
+        .argIf(SkipSigned,'--skip-signed');
+
+    azuresigntool.exec();
+}
+
+//Taken from https://www.becomebetterprogrammer.com/check-for-empty-string-javascript-typescript/
+const isEmptyString = (data: string): boolean => typeof data === "string" && data.trim().length == 0;
 
 run();
